dc6a643bd76dfffd4c6b618f21d952ff8dd8f191
Kristopher Lea <kristopherlea187@gmail.com>
2020-11-09 10:19:37 -0600

    another test

---
+++ src/test/java/test2.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class test2 {
+
+    @Test
+    public void test2(){
+        assertEquals(7,7);
+    }
+}
